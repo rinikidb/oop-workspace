512dbb482741afa63ea47776128d3a35add26412
root <root@DESKTOP-B46C48N>
2024-02-26 22:44:29 +1030

    workshop1

--- main.cpp
+++ main.cpp
@@ -5,17 +5,11 @@
 extern int count(int[], int);
 
 int main() {
-    //test case1
+    //test cases
+    //1. array[5] = {1,2,6,7,8}; expected output: The number is: 1
+    //2. array[6] = {2,4,6,7,9,10}; expected output: The number is: 4
     int array[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array, 5) << std::endl;
-
-    //test case2
-    int array1[6] = {4,2,6,7,8,9};
-    std::cout << "The number is: " << count(array1, 6) << std::endl;
-    
-    //test case3
-    int array2[5] = {1,5,6,7,9};
-    std::cout << "The number is: " << count(array2, 5) << std::endl;
     
     return 0;
 }
