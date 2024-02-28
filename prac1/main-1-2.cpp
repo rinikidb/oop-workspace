3fd336bd18e4758ed98a36d06f739fd96147ac0a
root <root@DESKTOP-B46C48N>
2024-02-28 14:14:30 +1030

    modifications

--- prac1/main-1-2.cpp
+++ prac1/main-1-2.cpp
@@ -4,11 +4,11 @@ extern double array_mean(int array[], int n);
 
 int main(){
     //test cases
-    //1. array[5] = {1,2,6,7,8}; expected output: The number is: 6
-    //2. array[6] = {2,4,6,7,9,10}; expected output: The number is: 6.33
-    //3. array[] = { } where n=-1; expected output: The number is: 0.0
+    //1. array[5] = {1,2,6,7,8}; expected output: The avg is: 6
+    //2. array[6] = {2,4,6,7,9,10}; expected output: The avg is: 6.33
+    //3. array[] is any array where n=-1; expected output: The avg is: 0.0
     int array[5] = {4,5,6,7,8};
-    std::cout << "The average of all the elements in the array is: " << array_mean(array, -1) << std::endl;
+    std::cout << "The average of all the elements in the array is: " << array_mean(array, 5) << std::endl;
     
     return 0;
 }
