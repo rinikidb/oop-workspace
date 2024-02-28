a59a49b5b2cedfa664a8096d87780de069d523b6
root <root@DESKTOP-B46C48N>
2024-02-28 16:54:50 +1030

    modifications and prac1-2-2

--- prac1/main-2-1.cpp
+++ prac1/main-2-1.cpp
@@ -4,7 +4,7 @@ extern int min_element(int array[], int n);
 
 int main(){
     int array[5] = {4,5,6,7,8};
-    std::cout << "The average of all the elements in the array is: " << min_element(array, 5) << std::endl;
+    std::cout << "The min of all the elements in the array is: " << min_element(array, 5) << std::endl;
     
     return 0;
 }
