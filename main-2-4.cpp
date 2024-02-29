e9f9f811b4d8412acc8386e19125a1f8d564c284
root <root@DESKTOP-B46C48N>
2024-02-29 22:41:34 +1030

    ok

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -3,7 +3,7 @@
 extern bool is_ascending(int array[], int n);
 
 int main(){
-    int array[10] = {2,3,6,8,5,4,2,9,0,1};
+    int array[10] = {2,3,4,5,5,6,7,8,9,10};
     is_ascending(array, 10);
     return 0;
 }
