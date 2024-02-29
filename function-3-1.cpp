f678395c6cc438b168a0e3d474028c6a6742211b
root <root@DESKTOP-B46C48N>
2024-03-01 00:05:09 +1030

    ok

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -4,29 +4,48 @@ bool is_fanarray(int array[], int n){
     int answerasc = 0;
     int answerpal = 0;
     if (n > 1){
-        for (int i = 0; i < (n-1)/2; i++){
-            if (array[i] <= array[i+1]){
-                answerasc = 1;
-            }else{
-                break;
-            }
-        }
-        if (answerasc == 1){
-            for (int i = 0; i < (n-1)/2; i++){
-                if (array[i] == array[4-i]){
-                    answerpal = 1;
+        if (n % 2 == 0){
+            for (int i = 0; i < n/2; i++){
+                if (array[i] <= array[i+1]){
+                    answerasc = 1;
                 }else{
-                    break; 
+                    break;
                 }
             }
-        }        
+            if (answerasc == 1){
+                for (int i = 0; i < n/2; i++){
+                    if (array[i] == array[4-i]){
+                        answerpal = 1;
+                    }else{
+                        break; 
+                    }
+                }
+            }        
+        }
+        else {
+            for (int i = 0; i < (n-1)/2; i++){
+                if (array[i] <= array[i+1]){
+                    answerasc = 1;
+                }else{
+                    break;
+                }
+            }
+            if (answerasc == 1){
+                for (int i = 0; i < (n-1)/2; i++){
+                    if (array[i] == array[4-i]){
+                        answerpal = 1;
+                    }else{
+                        break; 
+                    }
+                }
+            }        
+        }
         
         if (answerpal == 1){
             return 1;
-        }
-        else{
+        }else{
             return 0;
-        }  
+        }
     }
     else {
         return 0;
