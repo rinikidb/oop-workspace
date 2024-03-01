ecfa75a7374302dcfeaf6dd9e47955c085c14140
root <root@DESKTOP-B46C48N>
2024-03-02 03:06:40 +1030

    ok

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -2,7 +2,7 @@
 
 double sum_even(double array[], int n){
     if (n > 1){
-        int sum;
+        double sum = 0;
         for (int i = 0; i < n; i++){
             if (i % 2 == 0){
                 sum += array[i];
