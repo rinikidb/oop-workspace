c28c614a40da99d8f7c96c638b8c876602a47c77
root <root@DESKTOP-B46C48N>
2024-03-08 02:35:45 +1030

    ok

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 int is_identity(int array[10][10]){
-    int answer;
+    int answer = 0;
     for (int i = 0; i < 10; i++){
             for (int j = 0; j < 10; j++){
                 if (i == j){
                     if (array[i][j] == 1){
-                        answer = 1;
-                    }else{
-                        answer = 0;
+                        answer += 1;
                     }
                 }
                 /*
@@ -23,21 +21,19 @@ int is_identity(int array[10][10]){
             }
     }
     
-    if (answer == 1){
+    if (answer == 10){
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < 10; j++){
                 if (i != j){
                     if (array[i][j] == 0){
-                        answer = 1;
-                    }else{
-                        answer = 0;
+                        answer += 1;
                     }
                 }
             }
         }
         
     }
-    if (answer == 1){
+    if (answer == 100){
         return 1; 
     }  
     else{
