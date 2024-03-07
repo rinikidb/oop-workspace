159a7c3ae43d52a7c6999496162487db91d7fc54
root <root@DESKTOP-B46C48N>
2024-03-07 19:11:11 +1030

    ok

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print_scaled(int array[3][3],int scale){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            cout << array[i][j]*3 << " ";
+            cout << array[i][j]*scale << " ";
         }
         cout << endl;
     }
