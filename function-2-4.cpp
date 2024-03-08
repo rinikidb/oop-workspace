e4c8f39bc59bb592a9b105c8cab887bb509d75de
root <root@DESKTOP-B46C48N>
2024-03-08 21:28:45 +1030

    ok

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int array_min(int integers[], int length){
-    int min = integers[0];
     if (length < 1){
         return -1;  
     }
     else{
+        int min = integers[0];
         for (int i = 0; i < length; i++){
                 if (integers[i] < min){
-                    min = integers[0];
+                    min = integers[i];
                 }
         }
         return min;    
@@ -17,14 +17,14 @@ int array_min(int integers[], int length){
 }
 
 int array_max(int integers[], int length){
-    int max = integers[0];
     if (length < 1){
         return -1;
     }
     else{
+        int max = integers[0];
         for (int i = 0; i < length; i++){
                 if (integers[i] > max){
-                    max = integers[0];
+                    max = integers[i];
                 }
         }
         return max;
