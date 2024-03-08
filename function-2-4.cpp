c98ab00122b6425a12e6fae9b1278699cb38c724
root <root@DESKTOP-B46C48N>
2024-03-08 21:07:21 +1030

    ok

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -4,41 +4,42 @@ using namespace std;
 int array_min(int integers[], int length){
     int min = integers[0];
     if (length < 1){
-        for (int i = 0; i < length; i++){
-            if (integers[i] < min){
-                min = integers[0];
-            }
-        }
-        return min; 
+        return -1;  
     }
     else{
-        return -1;
+        for (int i = 0; i < length; i++){
+                if (integers[i] < min){
+                    min = integers[0];
+                }
+        }
+        return min;    
     }
 }
 
 int array_max(int integers[], int length){
     int max = integers[0];
     if (length < 1){
-        for (int i = 0; i < length; i++){
-            if (integers[i] > max){
-                max = integers[0];
-            }
-        }
-        return max;
+        return -1;
     }
     else{
-        return -1;
+        for (int i = 0; i < length; i++){
+                if (integers[i] > max){
+                    max = integers[0];
+                }
+        }
+        return max;
     }
      
 }
 
 int sum_min_max(int integers[], int length){
+    int max_element = array_max(integers, length);
+    int min_element = array_min(integers, length);
+    int sum = min_element + max_element;
     if (length < 1){
         return -1;
     }
     else{
-        int max_element = array_max(integers, length);
-        int min_element = array_min(integers, length);
-        return (min_element + max_element);
+        return sum;
     }
 }
