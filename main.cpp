a012f07e3caba95f9600cda65fc91bf188232289
root <root@DESKTOP-B46C48N>
2024-03-12 16:40:39 +1030

    ok

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "workshop.h"
 using namespace std;
 
 extern void changeValue(double* x);
---
+++ workshop.h
@@ -0,0 +1,4 @@
+extern void changeValue(double* x);
+extern void printArray(double* a, int n);
+extern double arrayMax(double* a, int n);
+extern double* dynamicArray(int n, double m);
