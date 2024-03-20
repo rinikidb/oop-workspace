ed5ef949767166e54da75256c60983bb715563b5
root <root@DESKTOP-B46C48N>
2024-03-20 16:37:38 +1030

    ok

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -8,7 +8,7 @@ PersonList createPersonList(int n){
     array.people = new Person[n];
 
     for (int i = 0; i < n; i++){
-        array.people[i].name = "John Doe";
+        array.people[i].name = "Jane Doe";
         array.people[i].age = 1;
     }
     return array;
