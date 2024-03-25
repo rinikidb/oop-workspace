1af53fd6d16b3c9d4dfeca54b417d3af0aaef5d6
root <root@DESKTOP-B46C48N>
2024-03-25 16:58:23 +1030

    ok

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -16,6 +16,7 @@ Orchestra::Orchestra(int size){
 
 // returns the number of musicians who have joined the orchestra
 int Orchestra::get_current_number_of_members(){
+    current_size = sizeof(*members);
     return current_size;
 }
 
