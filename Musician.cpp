939f9167936c11afb9edc123b5188639949a761e
root <root@DESKTOP-B46C48N>
2024-03-25 15:31:30 +1030

    ok

---
+++ Musician.cpp
@@ -0,0 +1,21 @@
+#include "Musician.h"
+
+Musician::Musician(){
+    instrument = "null";
+    experience = 0;
+}
+
+Musician::Musician(std::string instrument, int experience){
+    this->instrument = instrument;
+    this->experience = experience;
+}
+
+std::string Musician::get_instrument(){
+    return instrument;
+}
+
+int Musician::get_experience(){
+    return experience;
+}
+
+
