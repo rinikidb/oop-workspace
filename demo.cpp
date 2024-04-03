e4e1b6ec5e5d41e71dcc7716ec36278756db2ed8
root <root@DESKTOP-B46C48N>
2024-04-03 16:06:25 +1030

    ok

--- demo.cpp
+++ demo.cpp
@@ -18,16 +18,33 @@ class B : public A {
     private:
         double x;
     public:
-        double fool(){
+        void set_x(double a_x){
+            this->x = a_x;
+        }
+        void set_a(int new_a){
+            if(new_a < 0){
+                a = 0;
+            }
+            else{
+                a = new_a;
+            }
+        }
+        double foo(){
             return x*a;
         }
 };
 
 int main(){
 
+    A a;
     B b;
 
+    a.set_a(-50);
+    cout << a.get_a() << endl;
     b.set_a(10);
+    b.set_x(0.2);
+
+    cout << b.foo() << endl;
 
     return 0;
 }
