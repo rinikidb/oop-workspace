d25d0483a4a637a820ea11fe091d9ea334c39975
root <root@DESKTOP-B46C48N>
2024-04-05 17:28:46 +1030

    ok

--- Motorbike.cpp
+++ Motorbike.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 int Motorbike::getParkingDuration(){
-            /*
+            
             currentTime = time(nullptr);
-            duration = timeOfEntry - currentTime;
+            duration = currentTime - timeOfEntry;
             //after reduction
             duration = duration - (0.1*duration);
             return duration;
-            */
-
+            
+            /*
             time_t currentTime = time(nullptr);
             int duration = difftime(currentTime, timeOfEntry);
             return duration * 0.9;
+            */
         }
