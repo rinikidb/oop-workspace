7a47498ec5cb1df37b44ac581b32bbdfbc341ee6
root <root@DESKTOP-B46C48N>
2024-04-05 17:26:29 +1030

    ok

--- Car.cpp
+++ Car.cpp
@@ -5,9 +5,15 @@
 using namespace std;
 
 int Car::getParkingDuration(){
+            /*
             currentTime = time(nullptr);
             duration = timeOfEntry - currentTime;
             //after reduction
             duration = duration - (0.1*duration);
             return duration;
+            */
+
+            time_t currentTime = time(nullptr);
+            int duration = difftime(currentTime, timeOfEntry);
+            return duration * 0.9;
         }
