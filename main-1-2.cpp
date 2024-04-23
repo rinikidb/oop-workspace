9722eb1513f72475ac1e8e3a66f60bd73665d7be
root <root@DESKTOP-B46C48N>
2024-04-24 01:53:00 +0930

    ok

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main(){
 
     Fridge Fridge(300,400);
+    cout << "Original Volume: " << Fridge.getVolume() << " litres" << endl;
+    cout << "Original Consumption: " << Fridge.getPowerConsumption() << " kWh/day" << endl;
     
-
-
-
+    Fridge.setVolume(500);
+    cout << "New Volume: " << Fridge.getVolume() << " litres" << endl;
+    cout << "New Consumption: " << Fridge.getPowerConsumption() << " kWh/day" << endl;
 
     return 0;
 }
