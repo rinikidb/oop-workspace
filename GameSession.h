4e66712786a4f16a08824a14137eeb0a4398f2bc
root <root@DESKTOP-B46C48N>
2024-05-29 16:44:16 +0930

    ok

--- GameSession.h
+++ GameSession.h
@@ -63,13 +63,10 @@ class GameSession{
                         }
                     }
                 }
-                if(i == maxCycles){
-                    cout << "Maximum number of cycles reached. Game over." << endl;
-                }
 
             }
-
-             
+            cout << "Maximum number of cycles reached. Game over." << endl;
+  
         }
 };
 
